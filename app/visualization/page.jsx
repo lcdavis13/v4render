8136d37a88084e423e4b48adfd99081b6fb89f8d
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2023-11-22 00:50:26 -1000

    Plotted a subset of contours data - plotly.js

--- app/visualization/page.jsx
+++ app/visualization/page.jsx
@@ -1,19 +1,20 @@
 "use client";
-import { useState } from "react";
-import rfsData from "rfs.json";
+import React, { useState, useEffect } from "react";
+import data from '../../data/xy_subset_sample.json'; // Adjust the path as needed
 import PlotCell2D from "@components/PlotCell2D";
 
-// WRITE YOUR JS CODE HERE
+const VisualizationPage = () => {
+    const [contours, setContours] = useState([]);
 
-const Visualization = () => {
-  const [data, setData] = useState(rfsData);
+    useEffect(() => {
+        setContours(data);
+    }, []);
 
-  return (
-    <section className="w-full flex-center flex-col">
-      {/* WRITE YOUR JSX CODE HERE TO SHOW ON THE SCREEN */}
-      <PlotCell2D />
-    </section>
-  );
+    return (
+        <section className="w-full flex-center flex-col">
+            <PlotCell2D depthsToPlot={[1, 2, 3]} numRings={5} />
+        </section>
+    );
 };
 
-export default Visualization;
+export default VisualizationPage;
--- components/PlotCell2D.jsx
+++ components/PlotCell2D.jsx
@@ -1,134 +1,165 @@
-import React from 'react';
-import { ResponsiveLine } from '@nivo/line';
-import rfsData from "rfs.json";
+import React, {useState, useEffect} from 'react';
+import Plot from 'react-plotly.js';
+import {interpolateViridis} from 'd3-scale-chromatic';
+import contoursData from '../data/xy_subset_sample.json';
 
-function readContoursFromJson(jsonData) {
-  const contours = [];
-  jsonData.forEach(cellData => {
-    Object.values(cellData).forEach(contoursData => {
-      Object.values(contoursData).forEach(contourData => {
-        if ("point" in contourData) {
-          contours.push(contourData["point"]);
+
+function organizeContoursByDepth(contoursData) {
+    const contoursByDepth = {};
+
+    contoursData.forEach(group => {
+        if (group.contour && Array.isArray(group.contour)) {
+            group.contour.forEach(point => {
+                const depth = point.depth;
+                if (!contoursByDepth[depth]) {
+                    contoursByDepth[depth] = [];
+                }
+                contoursByDepth[depth].push(point);
+            });
+        } else {
+            console.log('Contour is not an array or does not exist:', group.contour);
         }
-      });
     });
-  });
-  return contours;
+
+    console.log('Contours organized by depth:', contoursByDepth);
+    return contoursByDepth;
 }
 
-function organizeContoursByDepth(contours) {
-  const contoursByDepth = {};
-  contours.forEach(contour => {
-    const depth = parseFloat(contour[2]);
-    if (!contoursByDepth[depth]) {
-      contoursByDepth[depth] = [];
+function PlotContoursWithDepths({contoursByDepth, depthsToPlot}) {
+    const traces = [];
+    depthsToPlot.forEach((depth, index) => {
+        const color = interpolateViridis(index / depthsToPlot.length);
+        const contourObjects = contoursByDepth[depth] || [];
+        contourObjects.forEach((contourObject, contourIndex) => {
+            if (typeof contourObject === 'object' && contourObject.hasOwnProperty('x') && contourObject.hasOwnProperty('y') && contourObject.hasOwnProperty('depth')) {
+                traces.push({
+                    x: contourObject.x,
+                    y: contourObject.y,
+                    type: 'scatter',
+                    mode: 'lines',
+                    line: {color},
+                    name: `Depth ${depth} - Contour ${contourIndex + 1}`
+                });
+                traces.push({
+                    x: -contourObject.x,
+                    y: contourObject.y,
+                    type: 'scatter',
+                    mode: 'lines',
+                    line: {color},
+                    showlegend: false
+                });
+            } else {
+                console.log(`Contour object at depth ${depth} - Contour ${contourIndex + 1} is not a valid object.`);
+            }
+        });
+    });
+    console.log("PlotContoursWithDepths done");
+    return traces; // Return the array of trace objects directly
+}
+
+function PlotContourCenters({contoursByDepth, depthsToPlot}) {
+    const traces = [];
+    depthsToPlot.forEach((depth, index) => {
+        const color = interpolateViridis(index / depthsToPlot.length);
+        const contourObjects = contoursByDepth[depth] || [];
+        contourObjects.forEach((contourObject, contourIndex) => {
+            if (typeof contourObject === 'object' && contourObject.hasOwnProperty('x') && contourObject.hasOwnProperty('y') && contourObject.hasOwnProperty('depth')) {
+                const centerX = contourObject.x;
+                const centerY = contourObject.y;
+                traces.push({
+                    x: [centerX],
+                    y: [centerY],
+                    type: 'scatter',
+                    mode: 'markers',
+                    marker: {color},
+                    name: `Center Depth ${depth} - Contour ${contourIndex + 1}`
+                });
+                traces.push({
+                    x: [-centerX],
+                    y: [centerY],
+                    type: 'scatter',
+                    mode: 'markers',
+                    marker: {color},
+                    showlegend: false
+                });
+            } else {
+                console.log(`Contour object at depth ${depth} - Contour ${contourIndex + 1} is not a valid object.`);
+            }
+        });
+    });
+    console.log("PlotContourCenters done");
+    return traces; // Return the array of trace objects directly
+}
+
+
+function addTargetRings(numRings, traces) {
+    const radmult = 2;
+    const theta = Array.from({length: 100}, (_, i) => i * (2 * Math.PI) / 100);
+
+    for (let i = 1; i <= numRings; i++) {
+        const x = theta.map(angle => i * Math.cos(angle) * radmult);
+        const y = theta.map(angle => i * Math.sin(angle) * radmult);
+
+        traces.push({
+            x: x,
+            y: y,
+            type: 'scatter',
+            mode: 'lines',
+            line: {color: 'pink', dash: 'dash', width: 1.5},
+            hoverinfo: 'none',
+            showlegend: false
+        });
     }
-    contoursByDepth[depth].push(contour);
-  });
-  return contoursByDepth;
 }
 
-function generateNivoData(contoursByDepth, depthsToPlot) {
-  return depthsToPlot.map((depth, index) => {
-    const contoursAtDepth = contoursByDepth[depth] || [];
-    const dataPoints = contoursAtDepth.map(contour => ({ x: contour[0], y: contour[1] }));
+function PlotCell2D({depthsToPlot = [], numRings = 0}) {
+    const [contoursByDepth, setContoursByDepth] = useState({});
 
-    return {
-      id: `Depth ${depth}`,
-      data: dataPoints,
+    useEffect(() => {
+        // Combine all contours from different keys into a single array
+        const allContours = Object.values(contoursData).flat();
+        console.log("allContours:", allContours); // Debugging console log
+        const organizedData = organizeContoursByDepth(allContours);
+        console.log("organizedData:", organizedData); // Debugging console log
+        setContoursByDepth(organizedData);
+    }, []);
+
+    // Initialize traces as an empty array
+    let traces = [];
+
+    // Generate traces for contours and contour centers
+    if (Object.keys(contoursByDepth).length > 0) {
+        const contoursTraces = PlotContoursWithDepths({contoursByDepth, depthsToPlot});
+        console.log("contoursTraces:", contoursTraces); // Debugging console log
+
+        const centersTraces = PlotContourCenters({contoursByDepth, depthsToPlot});
+        console.log("centersTraces:", centersTraces); // Debugging console log
+
+        // Ensure that both contoursTraces and centersTraces are arrays
+        if (Array.isArray(contoursTraces) && Array.isArray(centersTraces)) {
+            traces = [...contoursTraces, ...centersTraces];
+        } else {
+            console.error("contoursTraces or centersTraces is not an array.");
+        }
+
+        addTargetRings(numRings, traces);
+        console.log("traces after addTargetRings:", traces); // Debugging console log
+    }
+
+    // Define the layout for the plot
+    const layout = {
+        title: 'V4 Receptive Fields',
+        xaxis: {title: 'X'},
+        yaxis: {title: 'Y'},
+        legend: {
+            title: 'Legend',
+            items: depthsToPlot.map(depth => `Depth ${depth}`)
+        },
+        showlegend: true
     };
-  });
+
+    return <Plot data={traces} layout={layout}/>;
 }
 
-function addTargetRings(numRings) {
-  const radmult = 2;
-  const theta = new Array(100).fill().map((_, i) => (i / 100) * 2 * Math.PI);
-
-  const circles = [];
-
-  for (let i = 1; i <= numRings; i++) {
-    const x = theta.map(angle => i * Math.cos(angle) * radmult);
-    const y = theta.map(angle => i * Math.sin(angle) * radmult);
-
-    circles.push(
-      <circle
-        key={`circle-${i}`}
-        cx={x[0]}
-        cy={y[0]}
-        r={1.5}
-        fill="none"
-        stroke="pink"
-        strokeWidth={1.5}
-        strokeDasharray="5"
-      />
-    );
-  }
-
-  return circles;
-}
-
-function PlotCell2D() {
-  const depthsToPlot = [3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
-
-  const contours = readContoursFromJson(rfsData);
-  const contoursByDepth = organizeContoursByDepth(contours);
-  const nivoData = generateNivoData(contoursByDepth, depthsToPlot);
-  const circles = addTargetRings(10);
-
-  const chartProps = {
-    data: nivoData,
-    margin: { top: 50, right: 110, bottom: 50, left: 60 },
-    xScale: { type: 'linear' },
-    yScale: { type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false },
-    axisTop: null,
-    axisRight: null,
-    axisBottom: null,
-    axisLeft: null,
-    enableGridX: false,
-    enableGridY: false,
-    colors: 'nivo',
-    pointSize: 10,
-    pointColor: { from: 'color', modifiers: [] },
-    pointBorderWidth: 2,
-    pointBorderColor: { from: 'serieColor' },
-    pointLabelYOffset: -12,
-    useMesh: true,
-    legends: [
-      {
-        anchor: 'top-right',
-        direction: 'column',
-        justify: false,
-        translateX: 100,
-        translateY: 0,
-        itemsSpacing: 0,
-        itemDirection: 'left-to-right',
-        itemWidth: 80,
-        itemHeight: 20,
-        itemOpacity: 0.75,
-        symbolSize: 12,
-        symbolShape: 'circle',
-        symbolBorderColor: 'rgba(0, 0, 0, .5)',
-        effects: [
-          {
-            on: 'hover',
-            style: {
-              itemBackground: 'rgba(0, 0, 0, .03)',
-              itemOpacity: 1,
-            },
-          },
-        ],
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <svg width={500} height={500}>
-        {circles}
-      </svg>
-      <ResponsiveLine {...chartProps} />
-    </div>
-  );
-}
 
 export default PlotCell2D;

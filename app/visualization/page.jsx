3f4cc38b04174cd1ca77ea76a8481a3c77f85b38
acatarinaoaraujo <acoa_@hotmail.com>
2023-11-21 15:10:16 -1000

    add file to gitignore

--- app/visualization/page.jsx
+++ app/visualization/page.jsx
@@ -1,11 +1,17 @@
-{/* IMPORTS ON TOP */}
+"use client";
+import { useState } from "react";
+import rfsData from "rfs.json";
 
-{/* WRITE YOUR JAVASCRIPT CODE HERE */}
+// WRITE YOUR JS CODE HERE
 
-const Visualization = () => (
-  <section className="w-full flex-center flex-col">
-    {/* WRITE YOUR JSX CODE HERE */}
-  </section>
-);
+const Visualization = () => {
+  const [data, setData] = useState(rfsData);
+
+  return (
+    <section className="w-full flex-center flex-col">
+      {/* WRITE YOUR JSX CODE HERE TO SHOW ON THE SCREEN */}
+    </section>
+  );
+};
 
 export default Visualization;

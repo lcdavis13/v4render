ad4be5be29231085cb2b89d6b579018e78ba0514
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-11-27 11:19:39 -1000

    contours in 3D view

--- components/PlotCell3D.jsx
+++ components/PlotCell3D.jsx
@@ -3,66 +3,66 @@ import { Canvas, useFrame } from 'react-three-fiber';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import * as d3 from 'd3';
+import 'd3-scale';
 
 // Load your points data from the file
 import pointsData from '../data/xy_subset_sample.json';
 
-function flattenPointsData(data) {
-    const flattenedPoints = [];
+function extractContours(data) {
+    const contours = [];
 
     for (const key in data) {
         if (data.hasOwnProperty(key)) {
-            const contours = data[key];
-            contours.forEach((contour) => {
+            const contourGroup = data[key];
+            contourGroup.forEach((contour) => {
                 if (contour.contour && Array.isArray(contour.contour)) {
-                    contour.contour.forEach((point) => {
-                        flattenedPoints.push([point.x, point.y, point.depth]);
-                    });
+                    contours.push(contour.contour.map(({ x, y, depth }) => [x, y, depth]));
                 }
             });
         }
     }
 
-    return flattenedPoints;
+    return contours;
 }
 
-function Points({ points }) {
-    const pointsRef = useRef();
+function ContourLines({ contours }) {
+    const linesRef = useRef();
 
     useFrame(() => {
-        if (pointsRef.current) {
-            pointsRef.current.rotation.x += 0.005;
-            pointsRef.current.rotation.y += 0.005;
+        if (linesRef.current) {
+            linesRef.current.rotation.x += 0.005;
+            linesRef.current.rotation.y += 0.005;
         }
     });
 
-    // Extract depths from points
-    const depths = points.map((point) => point[2]);
+    // Extract depths from contours
+    const depths = contours.map((contour) => contour[0][2]);
 
     // Determine unique depths and assign colors
     const uniqueDepths = Array.from(new Set(depths));
     const colorScale = d3.scaleSequential(d3.interpolateViridis).domain([0, uniqueDepths.length - 1]);
 
-    // Create a BufferGeometry for the points
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(points.flat(), 3));
+    return (
+        <group>
+            {contours.map((contour, index) => {
+                const positions = contour.map(([x, y, depth]) => new THREE.Vector3(x, y, depth));
+                const colors = contour.map(([x, y, depth]) => new THREE.Color(colorScale(uniqueDepths.indexOf(depth))));
 
-    // Assign colors based on depths
-    const colors = depths.map((depth) => new THREE.Color(colorScale(uniqueDepths.indexOf(depth))).toArray());
-    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors.flat(), 3));
+                const geometry = new THREE.BufferGeometry().setFromPoints(positions);
+                geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors.flatMap(color => color.toArray()), 3));
 
-    // Create a Points object with the BufferGeometry and a PointsMaterial
-    const pointsObject = new THREE.Points(
-        geometry,
-        new THREE.PointsMaterial({ size: 2, vertexColors: true })
+                const material = new THREE.LineBasicMaterial({ vertexColors: true });
+
+                return (
+                    <line key={index} geometry={geometry} material={material} />
+                );
+            })}
+        </group>
     );
-
-    return <primitive object={pointsObject} />;
 }
 
 function PlotCell3D() {
-    const flattenedPoints = flattenPointsData(pointsData);
-    console.log(flattenedPoints);
+    const contours = extractContours(pointsData);
 
     return (
         <Canvas
@@ -71,7 +71,7 @@ function PlotCell3D() {
         >
             <axesHelper scale={[5, 5, 5]} />
             <ambientLight />
-            <Points points={flattenedPoints} />
+            <ContourLines contours={contours} />
             <OrbitControls />
             <directionalLight position={[10, 10, 5]} intensity={1} />
         </Canvas>
